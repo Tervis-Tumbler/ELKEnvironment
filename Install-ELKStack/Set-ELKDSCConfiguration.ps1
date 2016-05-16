71c08305719ac344a43f3a9f21f18750c07fdaf6
hambone124 <hperez@tervis.com>
2016-05-16 19:59:45 -0400

    Added Elasticsearch service installation

--- Install-ELKStack/Set-ELKDSCConfiguration.ps1
+++ Install-ELKStack/Set-ELKDSCConfiguration.ps1
@@ -1,7 +1,4 @@
-﻿
-
-Configuration ELKStackDSC
-{
+﻿Configuration ELKStackDSC {
    
     param (
         #[string[]]$ComputerName = $env:COMPUTERNAME + "." + $env:USERDNSDOMAIN
@@ -10,46 +7,92 @@ Configuration ELKStackDSC
 
    Import-DscResource -ModuleName cChoco
 
-   Node $ComputerName
-   {
-      LocalConfigurationManager
-      {
+   Node $ComputerName {
+      
+      LocalConfigurationManager {
         DebugMode = 'ForceModuleImport'
       }
-      cChocoInstaller installChoco
-      {
+
+      cChocoInstaller installChoco {
         InstallDir = "c:\choco"
       }
-      cChocoPackageInstaller installNSSM
-      {
+
+      <#
+
+      cChocoPackageInstaller installNSSM {
         Name = "nssm"
         DependsOn = "[cChocoInstaller]installChoco"
         Version = "2.24.0"
       }
-      cChocoPackageInstaller installJava
-      {
+
+      cChocoPackageInstaller installJava {
         Name = "jdk8"
         DependsOn = "[cChocoInstaller]installChoco"
+        Version = ""
       }
-      cChocoPackageInstaller installElasticsearch
-      {
+
+      #>
+
+      cChocoPackageInstaller installElasticsearch {
         Name = "elasticsearch"
-        DependsOn = "[cChocoPackageInstaller]installJava"
+        #DependsOn = "[cChocoPackageInstaller]installJava"
         Version = "2.3.1"
 
       }
-      cChocoPackageInstaller installLogstash
-      {
+
+      cChocoPackageInstaller installLogstash {
         Name = "logstash"
         DependsOn = "[cChocoPackageInstaller]installElasticsearch"
         Version = "2.3.1"
       }
      
-      cChocoPackageInstaller installKibana
-      {
+      cChocoPackageInstaller installKibana {
         Name = "kibana"
         Version = "4.5.0"
         DependsOn = "[cChocoPackageInstaller]installElasticsearch"
+      }
+
+      Script SetJavaHomeVariable {
+        GetScript = {
+            @{"JAVA_HOME"="$env:JAVA_HOME"}
+        }
+
+        SetScript = {
+            $JavaInstallPath = Join-Path $env:ProgramFiles java
+            $JavaVersion = Get-ChildItem -Path $JavaInstallPath -Name jdk* | Select-Object -Last 1
+            & cmd.exe /c setx /M JAVA_HOME $(Join-Path $JavaInstallPath $JavaVersion)
+
+        }
+
+        TestScript = {
+            if ($env:JAVA_HOME -ne $null) {
+                return $true
+            }
+            else {
+                return $false
+            }
+        }
+      }
+
+        Script InstallElasticsearchService {
+        GetScript = {
+            @{"Elastic Service Status"="$(Get-Service -Name elasticsearch-service-x64).Status"}
+        }
+
+        SetScript = {
+            $JavaInstallPath = Join-Path $env:ProgramFiles Java            $JavaVersion = Get-ChildItem -Path $JavaInstallPath -Name jdk* | Select-Object -Last 1            $ServiceInstallCommand = "c:\choco\lib\elasticsearch\tools\elasticsearch-2.3.1\bin\service.bat install"            $InstallCommand = "set JAVA_HOME=$(Join-Path $JavaInstallPath $JavaVersion)&&$ServiceInstallCommand"            & cmd.exe /C $InstallCommand
+
+        }
+
+        TestScript = {
+            $ElasticSearchService = Get-Service -Name elasticsearch-service* -ErrorAction SilentlyContinue
+            if ($ElasticSearchService -ne $null) {
+                return $true
+            }
+            else {
+                return $false
+            }
+        }
       }
  
     }
